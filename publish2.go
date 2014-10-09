9fa3cbdbc57ae4b6bf2299f0163ae2c7161821e3
Yesudeep Mangalapilly <yesudeep@google.com>
2014-10-09 22:07:29 +0530

    Allow the publisher function to display a value returned by the callback function.

--- publish2.go
+++ publish2.go
@@ -7,18 +7,20 @@ import (
 )
 
 // Publish returns the wait channel and closes the channel after publishing.
-func Publish(text string, delay time.Duration) (wait <-chan struct{}) {
+func Publish(foo func() string, delay time.Duration) (wait <-chan struct{}) {
 	ch := make(chan struct{})
 	go func() {
 		time.Sleep(delay)
-		fmt.Println("BREAKING NEWS:", text)
+		fmt.Println("BREAKING NEWS:", foo())
 		close(ch)
 	}()
 	return ch
 }
 
 func main() {
-	wait := Publish("Channels let goroutines communicate.", 5*time.Second)
+	wait := Publish(func() string {
+		return "Some news."
+	}, 5*time.Second)
 	fmt.Println("Waiting for the news...")
 	<-wait
 	fmt.Println("The news is out, time to leave.")
