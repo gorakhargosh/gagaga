bf2c626667ded3745592dcce6de026232e6b895d
Yesudeep Mangalapilly <yesudeep@google.com>
2014-10-19 13:31:01 +0530

    Foo.

--- outyet/outyet_test.go
+++ outyet/outyet_test.go
@@ -3,14 +3,42 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+	"time"
 )
 
+func TestIntegration(t *testing.T) {
+	status := statusHandler(404)
+	ts := httptest.NewServer(&status)
+	defer ts.Close()
+
+	s := NewServer("1.x", ts.URL, 1*time.Millisecond)
+	r, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	s.ServeHTTP(w, r)
+	if b := w.Body.String(); !strings.Contains(b, "No.") {
+		t.Errorf("body = %q, wanted no", b)
+	}
+
+	status = 200
+	time.Sleep(20 * time.Millisecond)
+	w = httptest.NewRecorder()
+	s.ServeHTTP(w, r)
+	if b := w.Body.String(); !strings.Contains(b, "YES!") {
+		t.Errorf("body = %q, wanted yes!", b)
+	}
+}
+
+type statusHandler int
+
+func (h *statusHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(int(*h))
+}
+
 func TestIsTagged(t *testing.T) {
-	status := 404
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(status)
-	}))
+	status := statusHandler(404)
+	ts := httptest.NewServer(&status)
 	defer ts.Close()
 
 	if isTagged(ts.URL) {
