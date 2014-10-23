b83b0a025f588db2969d43bfc5d4cc9a380fba49
Yesudeep Mangalapilly <yesudeep@google.com>
2014-10-23 11:05:42 +0530

    Enumerations.

---
+++ enumerations.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	Cyan = iota
+	Yellow
+	Magenta = 40
+	Blue    = iota
+	Green
+	Red
+	Black
+	Violet
+)
+
+const (
+	First = 2
+	Second
+)
+
+const (
+	StatusOn = 1 << iota
+	StatusOff
+	StatusInactive
+	StatusFound
+	StatusNotFound
+	StatusAccessDenied
+)
+
+func main() {
+	fmt.Println(Cyan, Magenta, Yellow, Blue, Green, Red, Black, Violet)
+	fmt.Println(First, Second)
+	fmt.Println(
+		StatusOn, StatusOff, StatusInactive, StatusFound,
+		StatusNotFound, StatusAccessDenied)
+	fmt.Println(math.Pi)
+}
